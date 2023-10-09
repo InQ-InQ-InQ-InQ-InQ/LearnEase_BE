2ce6b434f0bfdfc60fcecc5d931d217a082cfc54
SangBeom-Hahn <hsb990917@gmail.com>
2023-10-09 23:56:52 +0900

    project setting

---
+++ src/main/java/com/inq/learnease/LearnEaseBeApplication.java
@@ -0,0 +1,13 @@
+package com.inq.learnease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnEaseBeApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(LearnEaseBeApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/inq/learnease/LearnEaseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inq.learnease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnEaseBeApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
