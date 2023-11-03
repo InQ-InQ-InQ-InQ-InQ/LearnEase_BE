929d848668ebd4112345f99a4c8d29da724f1fa4
SangBeom-Hahn <hsb990917@gmail.com>
2023-11-03 12:45:00 +0900

    Feat(Email, User): 초기 엔터티 구축 및 테스트

---
+++ src/test/java/com/inq/learnease/entity/users/UserTest.java
@@ -0,0 +1,38 @@
+package com.inq.learnease.entity.users;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+    @Test
+    @DisplayName("회원의 닉네임을 수정한다.")
+    void changeNickname() {
+        // given
+        User dummyUser = new User(
+                Email.from("1@naver.com"),
+                "dummyPassword",
+                "learnEaseBe"
+        );
+        String expectedNickname = "changeNick";
+    
+        // when
+        dummyUser.changeNickname(expectedNickname);
+      
+        // then
+        assertThat(dummyUser.getNickname()).isEqualTo(expectedNickname);
+    }
+    
+    @Test
+    @DisplayName("회원을 생성한다.")
+    void construct() {
+        assertDoesNotThrow(() -> new User(
+                Email.from("1@naver.com"),
+                "dummyPassword",
+                "learnEaseBe"
+        ));
+    }
+}
