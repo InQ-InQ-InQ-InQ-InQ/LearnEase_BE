a6dc48515e10ca201806236ac26f37d7ac881272
SangBeom-Hahn <hsb990917@gmail.com>
2023-11-06 23:46:42 +0900

    Feat(Authorizatoin, JwtTokenProvider): jwt 토큰 발급 기능 구현

---
+++ src/main/java/com/inq/learnease/config/AuthenticationPrincipalConfig.java
@@ -0,0 +1,34 @@
+package com.inq.learnease.config;
+
+import com.inq.learnease.controller.auth.AuthenticationPrincipalArgumentResolver;
+import com.inq.learnease.controller.auth.LoginInterceptor;
+import com.inq.learnease.support.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
+    
+    private final JwtTokenProvider jwtTokenProvider;
+    
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor(jwtTokenProvider))
+                .order(1)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/login/auth")
+                .excludePathPatterns("/api/logout")
+                .excludePathPatterns("/api/user");
+    }
+    
+    @Override
+    public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new AuthenticationPrincipalArgumentResolver(jwtTokenProvider));
+    }
+}
---
+++ src/main/java/com/inq/learnease/support/JwtTokenProvider.java
@@ -0,0 +1,108 @@
+package com.inq.learnease.support;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Map;
+
+@Component
+public class JwtTokenProvider {
+    
+    private final SecretKey key;
+    private final long validityInMilliseconds;
+    
+    public JwtTokenProvider(@Value("${jwt.token.secret-key}") final String secretKey,
+                            @Value("${jwt.token.expire-length}") final long validityInMilliseconds) {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        this.validityInMilliseconds = validityInMilliseconds;
+    }
+    
+    public String createToken(Map<String, Object> payload) {
+        Claims claims = Jwts.claims(payload);
+        final Date now = new Date();
+        final Date validity = new Date(now.getTime() + validityInMilliseconds);
+        
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+    
+    public String getPayload(final String token) {
+        return tokenToJws(token)
+                .getBody()
+                .getSubject();
+    }
+    
+    public String getRolePayload(final String token) {
+        return tokenToJws(token)
+                .getBody()
+                .get("role")
+                .toString();
+    }
+    
+    public void validateAbleToken(final String token) {
+        try {
+            final Jws<Claims> claims = tokenToJws(token);
+            
+            validateExpiredToken(claims);
+        } catch (final JwtException | IllegalArgumentException e) {
+            throw new IllegalArgumentException(token);
+        }
+    }
+    
+    private Jws<Claims> tokenToJws(final String token) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+        } catch (final IllegalArgumentException | MalformedJwtException e) {
+            throw new IllegalArgumentException();
+        } catch (final SignatureException e) {
+            throw new IllegalArgumentException(token);
+        } catch (final ExpiredJwtException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+    
+    private void validateExpiredToken(final Jws<Claims> claims) {
+        if (claims.getBody().getExpiration().before(new Date())) {
+            throw new IllegalArgumentException();
+        }
+    }
+    
+    public static PayloadBuilder payloadBuilder() {
+        return new PayloadBuilder();
+    }
+    
+    public static class PayloadBuilder {
+        private final Claims claims;
+        
+        private PayloadBuilder() {
+            this.claims = Jwts.claims();
+        }
+        
+        public PayloadBuilder setSubject(String subject) {
+            claims.setSubject(subject);
+            return this;
+        }
+        
+        public PayloadBuilder put(String payload) {
+            claims.put("role", payload);
+            return this;
+        }
+        
+        public Map<String, Object> build() {
+            return claims;
+        }
+    }
+}
