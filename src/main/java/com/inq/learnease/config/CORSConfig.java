7d2ddff6c2773810ee81fb7d56eb0f79392dc0ce
piedra-de-flor <xxspdlqjxx@naver.com>
2024-02-26 19:10:50 +0900

    fix : CORS config 설정 추가

--- src/main/java/com/inq/learnease/config/CORSConfig.java
+++ src/main/java/com/inq/learnease/config/CORSConfig.java
@@ -9,6 +9,10 @@ public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
